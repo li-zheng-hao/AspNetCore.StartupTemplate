0ac5983b3afb5475fcd69126041b4c89ace4dafe
LiZhengHao <1263212577@qq.com>
2022-09-01 15:24:22 +0800

    更新项目结构

---
+++ AspNetCore.StartupTemplate.DbMigration/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace AspNetCore.StartupTemplate.DbMigration;
+
+public class Class1
+{
+}
--- AspNetCore.StartupTemplate.DbMigration/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace AspNetCore.StartupTemplate.DbMigration;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
